35c1a4a1a227729542d9c8a7d72be6a1e73937d1
Tabalbar <Tabalbar@hawaii.edu>
2019-04-03 14:53:57 -1000

    added functionality to footer
    needs work on foodiecraving link logo

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -13,10 +13,10 @@ class Footer extends React.Component {
             <Menu.Item>
               <Header inverted as='h6'>Created @ 2019</Header>
             </Menu.Item>
-            <Menu.Item fitted position="right"><Icon name="facebook f"/></Menu.Item>
-            <Menu.Item fitted><Icon name="twitter"/></Menu.Item>
-            <Menu.Item fitted><Icon name="pinterest"/></Menu.Item>
-            <Menu.Item fitted><Icon name="instagram"/></Menu.Item>
+            <Menu.Item fitted position="right" href="https://www.facebook.com/foodie.cravings.712"><Icon name="facebook f"/></Menu.Item>
+            <Menu.Item fitted href="https://twitter.com/foodiecravings1"><Icon name="twitter"/></Menu.Item>
+            <Menu.Item fitted href="https://www.pinterest.com/foodiecravings8179/"><Icon name="pinterest"/></Menu.Item>
+            <Menu.Item fitted href="https://www.instagram.com/fooodiecravings/?hl=en"><Icon name="instagram"/></Menu.Item>
             ) : ''}
           </Menu>
         </footer>

a445119b69aa9f7291101dd5c8f67838e59575ca
Alicia Leong <alicial8@hawaii.edu>
2019-04-09 11:35:48 -1000

    added background

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,33 +5,35 @@ import { Grid, Image} from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-        <Grid container columns={3}>
-          <Grid.Column>
-            <h1>Featured Item:</h1>
-            <Image className='landing-page-image' src="/images/laulau.png"/>
-            <h2>Lau Lau</h2>
-            <p>From: Helena's Hawaiian Food</p>
-            <p>Rating: 4.7 out of 5</p>
-            <p>Location: Off Campus</p>
-          </Grid.Column>
+        <div className='landing'>
+          <Grid container columns={3}>
+            <Grid.Column>
+              <h1>Featured Item:</h1>
+              <Image className='landing-page-image' src="/images/laulau.png"/>
+              <h2>Lau Lau</h2>
+              <p>From: Helena's Hawaiian Food</p>
+              <p>Rating: 4.7 out of 5</p>
+              <p>Location: Off Campus</p>
+            </Grid.Column>
 
-          <Grid.Column>
-            <h1>Top Rated Item:</h1>
-            <Image className='landing-page-image' src="/images/hamburger.png"/>
-            <h2>Hamburger</h2>
-            <p>From: L&L Drive Inn</p>
-            <p>Rating: 5 out of 5</p>
-            <p>Location: Paradise Palms</p>
-          </Grid.Column>
-          <Grid.Column>
-            <h1>Recently Added Item:</h1>
-            <Image className='landing-page-image' src="/images/gchicken.png"/>
-            <h2>Garlic Chicken</h2>
-            <p>From: Aja</p>
-            <p>Rating: 4 out of 5</p>
-            <p>Location: Campus Center</p>
-          </Grid.Column>
-        </Grid>
+            <Grid.Column>
+              <h1>Top Rated Item:</h1>
+              <Image className='landing-page-image' src="/images/hamburger.png"/>
+              <h2>Hamburger</h2>
+              <p>From: L&L Drive Inn</p>
+              <p>Rating: 5 out of 5</p>
+              <p>Location: Paradise Palms</p>
+            </Grid.Column>
+            <Grid.Column>
+              <h1>Recently Added Item:</h1>
+              <Image className='landing-page-image' src="/images/gchicken.png"/>
+              <h2>Garlic Chicken</h2>
+              <p>From: Aja</p>
+              <p>Rating: 4 out of 5</p>
+              <p>Location: Campus Center</p>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }

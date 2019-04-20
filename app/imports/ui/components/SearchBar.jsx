389daa2932ccc48a326b9e55fa6d1a30df070251
Tabalbar <Tabalbar@hawaii.edu>
2019-04-19 21:53:44 -1000

    changed

--- app/imports/ui/components/SearchBar.jsx
+++ app/imports/ui/components/SearchBar.jsx
@@ -1,10 +1,13 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
-import { Search, Grid } from 'semantic-ui-react'
+import { Search, Grid, Segment, Header } from 'semantic-ui-react'
 import { Foods } from '/imports/api/food/food';
+import faker from 'faker'
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from "meteor/meteor";
+
+const source = Meteor.settings.defaultFoods;
 
 class SearchBar extends Component {
 
@@ -17,22 +20,21 @@ class SearchBar extends Component {
 
   handleResultSelect = (e, { result }) => {
     this.setState({ value: result.name });
-    window.location.assign("/results");
+    // window.location.assign("/results");
   }
 
-  handleSearchChange = (e, { value }) => {{
-    this.setState({ isLoading: true, value });
-  }
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
 
     setTimeout(() => {
-      if (this.state.value.length < 1) return this.resetComponent();
+      if (this.state.value.length < 1) return this.resetComponent()
 
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i');
       const isMatch = result => re.test(result.name);
 
       this.setState({
         isLoading: false,
-        results: _.filter(this.props.foods, isMatch),
+        results: _.filter(this.props.stuff.name, isMatch),
       })
     }, 300)
   };
@@ -56,6 +58,14 @@ class SearchBar extends Component {
                     value={value}
                     {...this.props}
             />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Segment>
+              <Header>State</Header>
+              <pre style={{ overflowX: 'auto' }}>{JSON.stringify(this.state, null, 2)}</pre>
+              <Header>Options</Header>
+              <pre style={{ overflowX: 'auto' }}>{JSON.stringify(Meteor.settings.defaultData, null, 2)}</pre>
+            </Segment>
           </Grid.Column>
         </Grid>
 
@@ -82,14 +92,17 @@ class SearchBar extends Component {
 /** Require an array of Food documents in the props. */
 SearchBar.propTypes = {
   results: PropTypes.array.isRequired,
+  foods: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
+  stuff: PropTypes.object.isRequired,
+
 };
 
 export default withTracker(() => {
   // Get access to Food documents.
-  const subscription = Meteor.subscribe('Food');
+  const subscription = Meteor.subscribe('Stuff');
   return {
-    results: Foods.find({}).fetch(),
+    stuffs: Stuffs.find({}).fetch(),
     foods: Foods.find({}).fetch(),
     ready: subscription.ready(),
   };

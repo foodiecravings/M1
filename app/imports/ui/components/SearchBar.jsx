ff71e2e7257acb713e971e73074981a3fcc1b9ee
josephpalma <joepalma08@hotmail.com>
2019-04-23 11:19:10 -1000

    Working filter and render from search bar

--- app/imports/ui/components/SearchBar.jsx
+++ app/imports/ui/components/SearchBar.jsx
@@ -1,44 +1,57 @@
-import _ from 'lodash'
-import React, { Component } from 'react'
-import { Search, Grid } from 'semantic-ui-react'
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Search, Grid } from 'semantic-ui-react';
 import { Foods } from '/imports/api/food/food';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Meteor } from "meteor/meteor";
+import { Meteor } from 'meteor/meteor';
+import { Redirect } from 'react-router';
 
 class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      results: [],
+      renderResults: false,
+    };
+  }
+
   componentWillMount() {
-    this.resetComponent()
+    this.resetComponent();
   }
 
-  resetComponent = () =>
-      this.setState({ isLoading: false, results: [], value: '' });
+  resetComponent = () => this.setState({ isLoading: false, results: [], value: '' });
 
-  handleResultSelect = (e, { result }) => {
-    this.setState({ value: result.name });
-    window.location.assign("/results");
-  }
+  handleResultSelect = () => {
+    this.setState({ renderResults: true });
+  };
 
   handleSearchChange = (e, { value }) => {{
     this.setState({ isLoading: true, value });
+
   }
 
     setTimeout(() => {
       if (this.state.value.length < 1) return this.resetComponent();
 
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i');
-      const isMatch = result => re.test(result.name);
+      const isMatch = result => re.test(result.title);
 
       this.setState({
         isLoading: false,
         results: _.filter(this.props.foods, isMatch),
-      })
-    }, 300)
+      });
+    }, 300);
   };
 
   render() {
     const { isLoading, value, results } = this.state;
+
+    if (this.state.renderResults) {
+      return <Redirect to='/results'/>;
+    }
 
     return (
         <Grid>
@@ -75,13 +88,13 @@ class SearchBar extends Component {
       </Grid>
       */
 
-    )
+    );
   }
 }
 
 /** Require an array of Food documents in the props. */
 SearchBar.propTypes = {
-  results: PropTypes.array.isRequired,
+  foods: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -89,7 +102,6 @@ export default withTracker(() => {
   // Get access to Food documents.
   const subscription = Meteor.subscribe('Food');
   return {
-    results: Foods.find({}).fetch(),
     foods: Foods.find({}).fetch(),
     ready: subscription.ready(),
   };

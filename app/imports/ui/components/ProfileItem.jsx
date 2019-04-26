cb988b88fc1113224f4b67914b7a82f81f7e0a35
Tabalbar <Tabalbar@hawaii.edu>
2019-04-25 20:03:46 -1000

    fixed broken image

--- app/imports/ui/components/ProfileItem.jsx
+++ app/imports/ui/components/ProfileItem.jsx
@@ -4,14 +4,17 @@ import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
 import { withTracker } from 'meteor/react-meteor-data';
+import ReactImageFallback from "react-image-fallback";
+
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class ProfileItem extends React.Component {
+
   render() {
     return (
         <Item.Group>
           <Item>
-            <Item.Image size='tiny' src={this.props.profile.photo} />
+            <ReactImageFallback className='avatarImage' size='tiny' src={this.props.profile.photo} fallbackImage='https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png'/>
             <Item.Content>
               <Item.Header>{this.props.profile.firstname} {this.props.profile.lastname}</Item.Header>
               <Item.Meta>Bio</Item.Meta>
